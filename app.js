9dd701f700b3e6f0fc3d1a47ce85511aa5b8379e
Nilanjan Pal <nilanjan.pal06@gmail.com>
2021-03-11 19:03:42 +0530

    Stripe payment integration added

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const app = express();
 const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(bodyParser.json());
-// app.use((req, res, next) => {
-//     res.header('Access-Control-Allow-Origin','*');
-//     res.header('Access-Control-Allow-Methods', 'GET, POST, PATCH, PUT, DELETE, OPTIONS');
-//     res.header('Access-Control-Allow-Headers', 'Origin, Access-Control-Allow-Methods, X-Requested-With, Content-Type, Accept, Authorization');
-//     next();
-// });
 app.use('/', router);
 
 const server = http.createServer(app);
--- routes/route.js
+++ routes/route.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const RazorPay = require('razorpay');
+const stripe = require('stripe')('sk_test_51IRBIKI5IyHb9l597gES1sdUxSwXThpfgHICQj8p3zOGzAG7bEHshHXN9hc6Cfg6WVm04xZvcZ7mfEQgsKQA9C8J00e69FEnnk');
 
 const apiKey = 'rzp_test_I8a4e1StpRmkhi';
 const apiSecret = 'ZYQz9pjy1Bfj8amPVfzS4a0D';
@@ -19,15 +20,52 @@ router.post('/createOrder', (req, res, next) => {
     .then(
         (response) => {
             resObj = {id: response.id,apiKey: apiKey }
-            res.send(resObj);
+            res.status(200).send(resObj);
         }
     )
     .catch(
         (error) => {
             console.log(error);
-            res.send(error);
+            res.status(400).send(error);
         }
     );
 });
+
+router.post('/checkoutStripe', (req, res, next) => {
+    const sessionRequest = {
+        payment_method_types: [... req.body.paymentTypes],
+        line_items: [
+            {
+                price_data: {
+                    currency: req.body.currency,
+                    product_data: {
+                        name: 'Test Product'
+                    },
+                    unit_amount: req.body.amount
+                },
+                quantity: req.body.quantity
+            }
+        ],
+        customer_email: req.body.email,
+        mode: 'payment',
+        success_url: 'http://localhost:4200/index.html',
+        cancel_url: 'http://localhost:4200/index.html',
+    };
+    console.log(sessionRequest);
+    stripe.checkout.sessions.create(sessionRequest)
+    .then(
+        (response) => {
+            console.log(response);
+            res.status(200).send({id: response.id});
+        }
+    )
+    .catch(
+        (error) => {
+            console.log(error);
+            res.status(401).send({error: error});
+        }
+    );
+    
+})
 
 module.exports = router;
